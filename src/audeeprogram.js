297151d7eaa393d87528da42ff0d477ecef9493d
Takami Hirata <26153825+takami-h@users.noreply.github.com>
2023-08-03 16:26:29 +0900

    feat: refactor

---
+++ src/audeeprogram.js
@@ -0,0 +1,78 @@
+const { chromium } = require('playwright');
+
+const ProgramPage = require('./pageobjects/ProgramPage');
+const EpisodePage = require('./pageobjects/EpisodePage');
+
+/**
+ * @typedef Channel
+ * @property {string} programUrl
+ * @property {string} title
+ * @property {string} description
+ * @property {string} keywords
+ * @property {string} author
+ * @property {string} thumbnailUrl
+ */
+
+/**
+ * @typedef Item
+ * @property {string} episodeUrl
+ * @property {string} title
+ * @property {string} publishedAt
+ * @property {string} audioUrl
+ */
+
+/**
+ * @typedef AudeeProgram
+ * @property {Channel} channel
+ * @property {Item[]} items
+ */
+
+/**
+ * fetch audee program data.
+ * @param {string} programUrl 
+ * @returns {Promise<AudeeProgram>} audee program data
+ */
+async function findProgram(programUrl) {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  const programPage = new ProgramPage(page);
+  await programPage.goto(programUrl);
+
+  /** @type {Channel} */
+  const channel = {
+    programUrl,
+    title: await programPage.title(),
+    description: await programPage.description(),
+    keywords: await programPage.keywords(),
+    author: await programPage.author(),
+    thumbnailUrl: await programPage.thumbnailUrl()
+  };
+
+  /** @type {Item[]} */
+  const items = [];
+  const urls = await programPage.allEpisodeUrls();
+
+  for (const url of urls) {
+    console.log(`start ${url}`);
+    const episodePage = new EpisodePage(page);
+    await episodePage.goto(url);
+
+    items.push({
+      episodeUrl: url,
+      title: await episodePage.title(),
+      publishedAt: await episodePage.publishedAt(),
+      audioUrl: await episodePage.audioUrl()
+    });
+  }
+  await browser.close();
+
+  return {
+    channel, items
+  };
+}
+
+module.exports = {
+  findProgram
+}
---
+++ src/feedwriter.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+
+/**
+ * write RSS Feed into file.
+ * @param {string} filename 
+ * @param {import('./audeeprogram').AudeeProgram} audeeProgram
+ */
+async function writeFeed(filename, {channel, items}) {
+  const itemsFeed = items.map(i => {
+    return `
+  <item>
+    <title>${i.title}</title>
+    <link>${i.episodeUrl}</link>
+    <pubDate>${i.publishedAt}</pubDate>
+    <description>${i.title}</description>
+    <guid isPermaLink="true">${i.episodeUrl}</guid>
+    <enclosure url="${i.audioUrl}" length="0" type="audio/mp3"/>
+    <itunes:author>${channel.author}</itunes:author>
+    <itunes:subtitle>${i.title}</itunes:subtitle>
+    <itunes:explicit>no</itunes:explicit>
+    <media:thumbnail url="${channel.thumbnailUrl}"/>
+  </item>`;
+  }).join('');
+
+  const feed = `<?xml version="1.0" encoding="UTF-8"?>
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:media="http://search.yahoo.com/mrss/" xml:lang="ja">
+    <channel>
+      <link>${channel.programUrl}</link>
+      <title>${channel.title}</title>
+      <description>${channel.description}</description>
+      <media:keywords>${channel.keywords}</media:keywords>
+      <language>ja</language>
+      <itunes:subtitle>${channel.description}</itunes:subtitle>
+      <itunes:author>${channel.author}</itunes:author>
+      <itunes:summary>${channel.description}</itunes:summary>
+      <itunes:keywords>${channel.keywords}</itunes:keywords>
+      <itunes:owner>
+        <itunes:name>${channel.author}</itunes:name>
+      </itunes:owner>
+      <itunes:image href="${channel.thumbnailUrl}" />
+      <itunes:explicit>no</itunes:explicit>
+      ${itemsFeed}
+    </channel>
+  </rss>
+    `;
+
+  await fs.writeFileSync('audee-podcast.xml', feed);
+}
+
+module.exports = {
+  writeFeed
+}
